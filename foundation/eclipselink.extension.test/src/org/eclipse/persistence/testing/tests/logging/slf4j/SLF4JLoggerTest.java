10f7d8a93c8aac9b0f4f2ea03809790e86e16190
vrossello <rossello.vicente@gmail.com>
2019-05-24 15:45:41 +0200

    upgrade (cutre) a asm 7.1

--- foundation/eclipselink.extension.test/src/org/eclipse/persistence/testing/tests/logging/slf4j/SLF4JLoggerTest.java
+++ foundation/eclipselink.extension.test/src/org/eclipse/persistence/testing/tests/logging/slf4j/SLF4JLoggerTest.java
@@ -15,115 +15,11 @@
 //      Tomas Kraus - EclipseLink 2.7 integration
 package org.eclipse.persistence.testing.tests.logging.slf4j;
 
-import static org.junit.Assert.assertEquals;
 
-import org.eclipse.persistence.internal.sessions.AbstractSession;
-import org.eclipse.persistence.logging.AbstractSessionLog;
-import org.eclipse.persistence.logging.slf4j.SLF4JLogger;
-import org.eclipse.persistence.sessions.Session;
-import org.eclipse.persistence.testing.framework.junit.LogTestExecution;
-import org.eclipse.persistence.testing.tests.junit.logging.LogLevelHelper;
-import org.eclipse.persistence.testing.tests.logging.LogTestSession;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.slf4j.LoggerFactory;
-
-import ch.qos.logback.classic.LoggerContext;
 
 /**
  * Unit tests for EclipseLink Logger bridge over SLF4J.
  */
 public class SLF4JLoggerTest {
-
-    /** Log the test being currently executed. */
-    @Rule public LogTestExecution logExecution = new LogTestExecution();
-
-    /** Current EclipseLink session. */
-    protected Session session;
-
-    /** Logback logger context. */
-    protected LoggerContext loggerContext;
-
-    /**
-     * Creates an instance of jUnit tests.
-     */
-    public SLF4JLoggerTest() {
-    }
-
-    /**
-     * Setup unit test.
-     */
-    @Before
-    public void setup() {
-        session = new LogTestSession();
-        session.setName("");
-        loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-        loggerContext.reset();
-    }
-
-    /**
-     * Clean this test suite.
-     */
-    @After
-    public void reset() {
-        session = null;
-        loggerContext = null;
-    }
-
-    /**
-     * Test {@code SLF4JLogger.getLevel()} method.
-     */
-    @Test
-    public void testGetLevel() {
-        final int configuredDefaultLevel = AbstractSessionLog.getDefaultLoggingLevel();
-        final SLF4JLogger logger = new SLF4JLogger();
-        final SLF4JLoggerHelper helper = new SLF4JLoggerHelper((AbstractSession)session, loggerContext, logger);
-        logger.setSession(session);
-        final int defaultLevel = logger.getLevel();
-        final String defaultLevelString = LogLevelHelper.logIdToName(defaultLevel);
-        assertEquals("SLF4J logging level " + defaultLevelString + " did not match configured level "
-        + Integer.toString(configuredDefaultLevel), configuredDefaultLevel, defaultLevel);
-        helper.testCategoryDefaultLevels(logger);
-    }
-
-    /**
-     * Test {@code SLF4JLogger.log(SessionLogEntry)} method with regular {@link String} message.
-     */
-    @Test
-    public void testLogMessage() {
-        final SLF4JLogger logger = new SLF4JLogger();
-        final SLF4JLoggerHelper helper = new SLF4JLoggerHelper(
-                (AbstractSession)session, loggerContext, logger);
-        logger.setSession(session);
-        helper.testLogMessage();
-    }
-
-    /**
-     * Test {@code SLF4JLogger.log(SessionLogEntry)} method with {@link Throwable} and stack trace logging turned off.
-     */
-    @Test
-    public void testLogExceptionWithoutStackTrace() {
-        final SLF4JLogger logger = new SLF4JLogger();
-        final SLF4JLoggerHelper helper = new SLF4JLoggerHelper(
-                (AbstractSession)session, loggerContext, logger);
-        logger.setSession(session);
-        logger.setShouldLogExceptionStackTrace(false);
-        helper.testLogExceptionWithoutStackTrace();
-    }
-
-    /**
-     * Test {@code SLF4JLogger.log(SessionLogEntry)} method with {@link Throwable} and stack trace logging turned on.
-     */
-    @Test
-    public void testLogExceptionStackTrace() {
-        final SLF4JLogger logger = new SLF4JLogger();
-        final SLF4JLoggerHelper helper = new SLF4JLoggerHelper(
-                (AbstractSession)session, loggerContext, logger);
-        logger.setSession(session);
-        logger.setShouldLogExceptionStackTrace(true);
-        helper.testLogExceptionStackTrace();
-    }
 
 }
